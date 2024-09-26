9c79a632ae03b488ac79d8136f13e57bcf32ba9b
Atsuko Coffey <atsucoffey@gmail.com>
2024-09-26 12:22:05 +0000

    fix: 403 error response page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -12,28 +12,27 @@ function userInformationHTML(user) {
                 </a>
             </div>
             <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
-        </div>
-    `;
+        </div>`;
 }
 
 function repoInformationHTML(repos) {
     if (repos.length == 0) {
-        retun `<div class="clearfix repo-list">No repos!</div>`
+        return `<div class="clearfix repo-list">No repos!</div>`;
     }
 
     var listItemsHTML = repos.map(function(repo) {
         return `<li>
-                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-        </li>`
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
     });
     return `<div class="clearfix repo-list">
-        <p>
-        <strong>Repo List:</strong>
-        </p>
-        <ul>
-            ${listItemsHTML.join("\n")}
-        </ul>
-    </div>`;
+                <p>
+                <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
 }
 
 function fetchGitHubInformation(event) {
@@ -45,12 +44,11 @@ function fetchGitHubInformation(event) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
-    
+
     $("#gh-user-data").html(
-        `<div id="loadrer">
-        <img src="assets/css/loader.gif" alt="loading...">
-        </div>`
-    );
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading...">
+        </div>`);
 
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`),
@@ -64,18 +62,14 @@ function fetchGitHubInformation(event) {
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
-                    `<h2>No info found for user ${username}</h2>`
-                );
+                    `<h2>No info found for user ${username}</h2>`);
             } else if (errorResponse.status === 403) {
-                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
-                $("#gh-user-data").html(
-                    `<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`
-                );
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
-                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
-                );
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
 }
